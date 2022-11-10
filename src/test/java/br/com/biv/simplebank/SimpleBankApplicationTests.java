f323e3b424ecf35f31e4218e27d8cfa26252ae45
Bruna Iriz <45856882+bruna-iriz@users.noreply.github.com>
2022-11-10 18:02:10 -0300

    commit inicial com as propriedades do BD

---
+++ src/test/java/br/com/biv/simplebank/SimpleBankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.biv.simplebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
